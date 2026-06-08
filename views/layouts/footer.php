536a9fb52c6557f15b1486bab7a101370231c352
unknown <mouhamadououry.diallo1@unchk.edu.sn>
2026-06-08 21:59:03 +0000

    page note etudiant

--- views/layouts/footer.php
+++ views/layouts/footer.php
@@ -1,6 +1,7 @@
 </div>
 
 <script src="../../assets/js/script.js"></script>
+<script src="../../assets/js/note.js"></script>
 
 </body>
 </html>
--- views/layouts/header.php
+++ views/layouts/header.php
@@ -10,6 +10,7 @@
     <link rel="stylesheet" href="../../assets/css/login.css">
     <link rel="stylesheet" href="../../assets/css/forms.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    <link rel="stylesheet" href="../../assets/css/note.css">
 </head>
 
 <body>
